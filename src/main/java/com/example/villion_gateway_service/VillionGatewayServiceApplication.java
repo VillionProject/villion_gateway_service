04dd29c32717de41d8148dacec6d557207749697
sesam-me <lsi6601@gmail.com>
2024-04-27 01:02:17 +0900

    t

---
+++ src/main/java/com/example/villion_gateway_service/VillionGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.villion_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillionGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillionGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/villion_gateway_service/VillionGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.villion_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillionGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
